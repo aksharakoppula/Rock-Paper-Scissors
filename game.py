55bd5746fa397320bfaf8a98cf50b684916de164
aksharakoppula <aadya.koppula@gmail.com>
2025-09-23 19:34:14 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,30 @@
 import random
+score=0
 game=["rock","paper","scissors"]
 for i in range(len(game)):
     print(game[i])
-print(random.choice(game))
+while True:
+    print("Pick Your Move")
+    playermove=input()
+    computerguess=random.choice(game)
+    if playermove=="rock" and computerguess=="scissors":
+        print("You Win!")
+        score+=1
+    if playermove==computerguess:
+        print("Tie, Play Again")
+    if playermove=="scissors" and computerguess=="paper":
+        print("You Win!")
+        score+=1
+    if playermove=="paper" and computerguess=="rock":
+        print("You Win!")
+        score+=1
+    if playermove=="rock" and computerguess=="paper":
+        print("You Lose")
+        score-=1
+    if playermove=="scissors" and computerguess=="rock":
+        print("You Lose")
+        score-=1
+    if playermove=="paper" and computerguess=="scissors":
+        print("You Lose")
+        score-=1
+    print("score:",score) 
